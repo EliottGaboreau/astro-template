c766d637a64fc85be0dd1dd303a829bfd348a603
Eliott Gaboreau <eliott.gab@gmail.com>
2024-09-08 12:51:34 +0200

    product pages

---
+++ src/lib/price.ts
@@ -0,0 +1,5 @@
+
+
+export function displayPrice(value_cents: number) {
+    return `${value_cents / 100} €`
+}
--- src/types.ts
+++ src/types.ts
@@ -12,12 +12,11 @@ export interface Website {
     url?: string | null;
     id?: string | null;
 }
-
-/**
- * This interface was referenced by `Config`'s JSON-Schema
- * via the `definition` "users".
- */
-export interface User {
+  /**
+   * This interface was referenced by `Config`'s JSON-Schema
+   * via the `definition` "users".
+   */
+  export interface User {
     id: number;
     organization?: (number | null) | Organization;
     organizationRole: 'admin' | 'editor';
@@ -35,26 +34,28 @@ export interface User {
     loginAttempts?: number | null;
     lockUntil?: string | null;
     password?: string | null;
-}
-/**
- * This interface was referenced by `Config`'s JSON-Schema
- * via the `definition` "organizations".
- */
-export interface Organization {
+  }
+  /**
+   * This interface was referenced by `Config`'s JSON-Schema
+   * via the `definition` "organizations".
+   */
+  export interface Organization {
     id: number;
     name?: string | null;
-    tags?: ('team_members' | 'theatre_company')[] | null;
     websites?:
-    | Website[]
-    | null;
+      | Website[]
+      | null;
+    team_members?: boolean | null;
+    theatre_company?: boolean | null;
+    ecommerce?: boolean | null;
     updatedAt: string;
     createdAt: string;
-}
-/**
- * This interface was referenced by `Config`'s JSON-Schema
- * via the `definition` "images".
- */
-export interface Image {
+  }
+  /**
+   * This interface was referenced by `Config`'s JSON-Schema
+   * via the `definition` "images".
+   */
+  export interface Image {
     id: number;
     alt?: string | null;
     organization?: (number | null) | Organization;
@@ -69,77 +70,99 @@ export interface Image {
     focalX?: number | null;
     focalY?: number | null;
     sizes?: {
-        thumbnail?: {
-            url?: string | null;
-            width?: number | null;
-            height?: number | null;
-            mimeType?: string | null;
-            filesize?: number | null;
-            filename?: string | null;
-        };
-        small?: {
-            url?: string | null;
-            width?: number | null;
-            height?: number | null;
-            mimeType?: string | null;
-            filesize?: number | null;
-            filename?: string | null;
-        };
-        medium?: {
-            url?: string | null;
-            width?: number | null;
-            height?: number | null;
-            mimeType?: string | null;
-            filesize?: number | null;
-            filename?: string | null;
-        };
-        large?: {
-            url?: string | null;
-            width?: number | null;
-            height?: number | null;
-            mimeType?: string | null;
-            filesize?: number | null;
-            filename?: string | null;
-        };
-        card?: {
-            url?: string | null;
-            width?: number | null;
-            height?: number | null;
-            mimeType?: string | null;
-            filesize?: number | null;
-            filename?: string | null;
-        };
+      thumbnail?: {
+        url?: string | null;
+        width?: number | null;
+        height?: number | null;
+        mimeType?: string | null;
+        filesize?: number | null;
+        filename?: string | null;
+      };
     };
-}
-/**
- * This interface was referenced by `Config`'s JSON-Schema
- * via the `definition` "pages".
- */
-export interface Page {
+  }
+  /**
+   * This interface was referenced by `Config`'s JSON-Schema
+   * via the `definition` "pages".
+   */
+  export interface Page {
     id: number;
     title: string;
     content?: {
-        root: {
-            type: string;
-            children: {
-                type: string;
-                version: number;
-                [k: string]: unknown;
-            }[];
-            direction: ('ltr' | 'rtl') | null;
-            format: 'left' | 'start' | 'center' | 'right' | 'end' | 'justify' | '';
-            indent: number;
-            version: number;
-        };
-        [k: string]: unknown;
+      root: {
+        type: string;
+        children: {
+          type: string;
+          version: number;
+          [k: string]: unknown;
+        }[];
+        direction: ('ltr' | 'rtl') | null;
+        format: 'left' | 'start' | 'center' | 'right' | 'end' | 'justify' | '';
+        indent: number;
+        version: number;
+      };
+      [k: string]: unknown;
     } | null;
     cover?: number | Image | null;
     organization?: (number | null) | Organization;
     slug?: string | null;
     meta?: {
-        title?: string | null;
-        description?: string | null;
+      title?: string | null;
+      description?: string | null;
     };
     updatedAt: string;
     createdAt: string;
-}
+  }
+  /**
+   * This interface was referenced by `Config`'s JSON-Schema
+   * via the `definition` "products".
+   */
+  export interface Product {
+    id: number;
+    organization?: (number | null) | Organization;
+    slug?: string | null;
+    title: string;
+    images?: (number | Image)[] | null;
+    price?: number | null;
+    updatedAt: string;
+    createdAt: string;
+  }
+  /**
+   * This interface was referenced by `Config`'s JSON-Schema
+   * via the `definition` "orders".
+   */
+  export interface Order {
+    id: number;
+    organization?: (number | null) | Organization;
+    customer?: (number | null) | Customer;
+    session_id?: string | null;
+    status?: ('pending' | 'paid' | 'fullfiled') | null;
+    total: number;
+    items?:
+      | {
+          product: number | Product;
+          price?: number | null;
+          quantity?: number | null;
+          id?: string | null;
+        }[]
+      | null;
+    updatedAt: string;
+    createdAt: string;
+  }
+  /**
+   * This interface was referenced by `Config`'s JSON-Schema
+   * via the `definition` "customers".
+   */
+  export interface Customer {
+    id: number;
+    organization?: (number | null) | Organization;
+    updatedAt: string;
+    createdAt: string;
+    email: string;
+    resetPasswordToken?: string | null;
+    resetPasswordExpiration?: string | null;
+    salt?: string | null;
+    hash?: string | null;
+    loginAttempts?: number | null;
+    lockUntil?: string | null;
+    password?: string | null;
+  }
