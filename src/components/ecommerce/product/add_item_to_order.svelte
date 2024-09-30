4ebaafd22a0bcfd424160e4fc47a810274d1dfc9
Eliott Gaboreau <eliott.gab@gmail.com>
2024-09-30 19:01:37 +0200

    draft for add to cart success

--- src/components/ecommerce/product/add_item_to_order.svelte
+++ src/components/ecommerce/product/add_item_to_order.svelte
@@ -3,16 +3,27 @@
 
     let { product } = $props();
 
+    let waiting = $state(false)
+    let success = $state(false)
+
     let { items, addItem } = getOrder();
 
     function handleClick() {
+        waiting = true
         addItem(product);
+        success = true
     }
 </script>
 
+{#if !success}
+    
 <button
     class="ring-inset p-1 ring-1 ring-gray-900 rounded-md hover:bg-gray-200 transition-colors"
     onclick={handleClick}
->
+    >
     {product.title}
 </button>
+{:else}
+<p>success</p>
+{/if}
+
