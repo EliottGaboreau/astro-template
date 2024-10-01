e083c0dda10267badfa3316937916c8fad3f4d5f
Eliott Gaboreau <eliott.gab@gmail.com>
2024-10-01 10:34:17 +0200

    add to cart css

--- src/components/ecommerce/product/add_item_to_order.svelte
+++ src/components/ecommerce/product/add_item_to_order.svelte
@@ -1,5 +1,6 @@
 <script>
     import { getOrder } from "@lib/order.svelte";
+    import {Check} from "svelte-lucide";
 
     let { product } = $props();
 
@@ -17,13 +18,24 @@
 
 {#if !success}
     
-<button
-    class="ring-inset p-1 ring-1 ring-gray-900 rounded-md hover:bg-gray-200 transition-colors"
-    onclick={handleClick}
-    >
-    {product.title}
-</button>
+<div class="inline-block">
+    <button
+        class="py-1 px-3 rounded-full bg-yellow-400 hover:bg-yellow-500 transition-colors"
+        onclick={handleClick}
+        >
+        Ajouter au panier
+    </button>
+</div>
 {:else}
-<p>success</p>
+<div class="bg-green-200 rounded-xl ring-offset-2 ring-offset-green-500 text-green-500 p-1.5 m-1">
+    <div class="p-1.5 outline-green-400 outline outline-2 rounded-lg">
+        <Check class="inline-block "/>
+        Ajouté au panier avec succès
+    </div>
+</div>
+<div class="flex flex-row">
+    <a class="m-1 py-1 px-3 rounded-full bg-yellow-400 hover:bg-yellow-500 transition-colors" href="/cart">Voir le Panier</a>
+    <a class="m-1 py-1 px-3 rounded-full ring-inset ring-1 ring-gray-900 hover:bg-gray-200 transition-colors" href="/products">Continuer</a>
+</div>
 {/if}
 
