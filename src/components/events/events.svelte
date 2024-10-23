ce7b798b334f42f966cf0200b4907ec9113e241a
Eliott Gaboreau <eliott.gab@gmail.com>
2024-10-23 16:00:19 +0200

    add events

---
+++ src/components/events/events.svelte
@@ -0,0 +1,20 @@
+<script>
+    let { events } = $props();
+    import EventRow from "./event_row.svelte";
+
+    $inspect(events);
+</script>
+
+<div class="max-w-6xl m-auto">
+    <h1 class="font-display text-5xl px-4 pt-4">Titre</h1>
+
+    <div class="m-4">
+        <table class="sm:w-full table-fixed">
+            <tbody>
+                {#each events as event}
+                    <EventRow {...event} />
+                {/each}
+            </tbody>
+        </table>
+    </div>
+</div>
